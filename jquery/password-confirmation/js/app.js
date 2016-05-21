ba916a9a7904a0bb21d2f8ce39519eb903388153
Nick Davenport <nick.davenport@lfpl.org>
2016-05-20 22:27:08 -0400

    Hide password hint with valid password
    When a valid password is entered the password hint is hidden, otherwise it is
    shown.

--- jquery/password-confirmation/js/app.js
+++ jquery/password-confirmation/js/app.js
@@ -2,12 +2,26 @@
 // Solution: Hide and show them at appropriate times
 
 // Hide hints
+$("form span").hide();
+
+var $in_passwd = $("#password");
+var $hint_passwd = $("#password-hint");
+
+function passwordEvent(){
+  // Is password valid?
+  if ($in_passwd.val().length > 8){
+    // Hide hint if valid
+    $hint_passwd.hide();
+  } else {
+    // else show hint
+    $hint_passwd.show();
+  }
+}
+
+
 
 // When event happens on password input
-  // Is password valid?
-    // Hide hint if valid
-
-    // else show hint
+$in_passwd.change(passwordEvent).keyup(passwordEvent);
 
 
 // When event happens on confirmation
