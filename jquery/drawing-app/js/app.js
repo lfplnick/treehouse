c37db107339218f7d96b4f68518a7427f6a9f1d0
Nick Davenport <nick.davenport@lfpl.org>
2016-05-21 21:44:55 -0500

    Define problem and outline solution

---
+++ jquery/drawing-app/js/app.js
@@ -0,0 +1,19 @@
+// Problem: No user interaction causes no change to application
+// Solution: When user interacts, cause changes appropriately
+
+// when clidking on control list items
+  // deselect sibling elements
+  // select clicked element
+
+// when new color is clicked
+  // show or hide color select
+
+// when color sliders change
+  // update new color span
+
+// when add color is clicked
+  // append color to control ul
+  // select new color
+
+// on mouse events on canvas
+  // draw lines
