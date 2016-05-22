7380b91f46c22a850d94df4b639f435fc86d12ae
Nick Davenport <nick.davenport@lfpl.org>
2016-05-21 22:04:33 -0500

    Update selected color when clicked
    When color swatch is clicked it becomes the selected color.

--- jquery/drawing-app/js/app.js
+++ jquery/drawing-app/js/app.js
@@ -1,9 +1,22 @@
 // Problem: No user interaction causes no change to application
 // Solution: When user interacts, cause changes appropriately
 
-// when clidking on control list items
-  // deselect sibling elements
-  // select clicked element
+var $colors = $(".controls ul li");
+
+
+
+// when clicking on control list items
+$colors.click(function(){
+  if (!$(this).hasClass("selected")){
+    // deselect sibling elements
+    $colors.each(function(){
+      $(this).removeClass("selected");
+    });
+
+    // select clicked element
+    $(this).addClass("selected");
+  }
+});
 
 // when new color is clicked
   // show or hide color select
