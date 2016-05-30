1a6a98a6928b3c623765e2c21bfadae3e01d2c61
Nick Davenport <nick.davenport@lfpl.org>
2016-05-30 01:18:58 +0000

    Bind event handlers to task buttons

--- interactive-js/js/app.js
+++ interactive-js/js/app.js
@@ -1,13 +1,14 @@
 // Problem: User interaction doesn't provide desired results
 // Solution: Add interactivity so the user can manage daily tasks
 
-var taskInput; // new-task
-var addButton; // first button
-var incompleteTasksHolder; // #incomplete-tasks
-var completetedTasksHolder; // #completed-tasks
+var taskInput = document.getElementById("new-task"); // new-task
+var addButton = document.getElementsByTagName("button")[0]; // first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); // #incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks"); // #completed-tasks
 
 // Add a new task
-var addTask = function{
+var addTask = function(){
+  console.log("Add task...");
   // When button is pressed
   // create a new list item with text from new task
     // input (checkbox)
@@ -20,6 +21,7 @@ var addTask = function{
 
 // Edit existing task
 var editTask = function(){
+  console.log("Edit task...");
   // when edit button is pressed
     // if class of parent is .editMode
       // switch from .editMode
@@ -32,18 +34,54 @@ var editTask = function(){
 
 // Delete an existing task
 var deleteTask = function(){
+  console.log("Delete task...");
   // when delete button is pressed
     // remove the parent li from the ul
 }
 
 // Mark task as complete
-var taskCompleted = function{
+var taskCompleted = function(){
+  console.log("Task complete...");
   // when checkbox is checked
     // append the task li to the #completed-tasks
 }
 
 // Mark a task as incomplete
-var taskIncomplete = function{
+var taskIncomplete = function(){
+  console.log("Task incomplete...");
   // when checkbox is unchecked
     // append task li to #incomplete-tasks
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler){
+  console.log("bind list item events...");
+  // select taskListItem's children
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  var deleteButton = taskListItem.querySelector("button.delete");
+
+  // bind editTask to edit button
+  editButton.onclick = editTask;
+
+  // bind deleteTask to the delete button
+  deleteButton.onclick = deleteTask;
+
+  // bind checkBoxEventHandler to checkbox
+  checkBox.onchange = checkBoxEventHandler;
+}
+
+
+// Set the click handler to the addTask function
+addButton.onclick = addTask;
+
+// cycle over incompleteTasksHolder ul list items
+for (var i = 0; i < incompleteTasksHolder.children.length; i++){
+    // bind events to list item's children (taskCompleted)
+  bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+
+// cycle over completedTasksHolder ul list items
+for (var i = 0; i < completedTasksHolder.children.length; i++){
+    // bind events to list item's children (taskIncomplete)
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
