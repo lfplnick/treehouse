3376491b012267d852fb3d58dcf56b9603599b9f
Nick Davenport <nick.davenport@lfpl.org>
2016-05-31 02:37:28 +0000

    Show room status on intranet home page

--- ajax/employee-widget/js/widget.js
+++ ajax/employee-widget/js/widget.js
@@ -1,4 +1,6 @@
 var xhr = new XMLHttpRequest();
+var xhr_rooms = new XMLHttpRequest();
+
 xhr.onreadystatechange = function(){
   if(xhr.readyState === 4){
     var employees = JSON.parse(xhr.responseText);
@@ -16,5 +18,28 @@ xhr.onreadystatechange = function(){
   }
   document.getElementById('employeeList').innerHTML = statusHTML;
 };
+
+xhr_rooms.onreadystatechange = function(){
+  if (xhr_rooms.readyState === 4){
+    if (xhr_rooms.status === 200){
+      var rooms = JSON.parse(xhr_rooms.responseText);
+      var statusHTML = '<ul class="rooms">';
+      for (var i = 0; i < rooms.length; i++) {
+        if (rooms[i].available === true) {
+          statusHTML += '<li class="empty">';
+        } else {
+          statusHTML += '<li class="full">';
+        }
+        statusHTML += rooms[i].room + '</li>'
+      }
+      statusHTML += '</ul>';
+      document.getElementById('roomList').innerHTML = statusHTML;
+    }
+  }
+};
+
 xhr.open('GET', 'data/employees.json');
 xhr.send();
+
+xhr_rooms.open('GET', 'data/rooms.json');
+xhr_rooms.send();
