ec3735b76504818167526a26c833aafff9bad473
Nick Davenport <nick.davenport@lfpl.org>
2016-06-24 20:29:58 -0400

    Remove synchronous AJAX call
    Once quiz object is pulled from external json file it is passed to initQuestions
    which handles adding the questions to the Quiz and loading the first question.

--- objects/quiz-project/js/quiz.js
+++ objects/quiz-project/js/quiz.js
@@ -1,11 +1,29 @@
 function Quiz (url_quiz) {
   this.correct   = 0;
   this.incorrect = 0;
-  this.questions = JSON.parse($.ajax({
-    "url" : url_quiz,
-    "async" : false,
-    "dataType" : "json"
-  }).responseText);
-  this.q_total = this.questions.length;
+  this.questions = [];
+  this.q_total   = 0;
   this.q_current = 0;
+
+
+  var thisQuiz = this;
+  $.ajax({
+    "url" : url_quiz,
+    "dataType" : "json",
+  }).success(function(quiz){
+    thisQuiz.initQuestions(quiz);
+  });
 }
+
+Quiz.prototype = {
+  initQuestions: function(quiz){
+    for (var i = 0; i < quiz.length; i++) {
+      var question = new Question(quiz[i].question, quiz[i].answers, quiz[i].correctAnswer);
+      this.questions.push(question);
+    }
+
+    this.q_total = this.questions.length;
+    updateProgress(this.q_current + 1, this.q_total);
+    loadQuestion(this.questions[0]);
+  }
+};
